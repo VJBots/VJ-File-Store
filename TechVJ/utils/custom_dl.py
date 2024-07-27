851a9d79d72b5241f324a437afcecc674cbc2df0
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-27 11:42:10 +0530

    Update custom_dl.py

--- TechVJ/utils/custom_dl.py
+++ TechVJ/utils/custom_dl.py
@@ -1,7 +1,7 @@
 import math
 import asyncio
 import logging
-from config import Var
+from config import LOG_CHANNEL
 from typing import Dict, Union
 from TechVJ.bot import work_loads
 from pyrogram import Client, utils, raw
@@ -49,7 +49,7 @@ class ByteStreamer:
         Generates the properties of a media file on a specific message.
         returns ths properties in a FIleId class.
         """
-        file_id = await get_file_ids(self.client, Var.BIN_CHANNEL, id)
+        file_id = await get_file_ids(self.client, LOG_CHANNEL, id)
         logging.debug(f"Generated file ID and Unique ID for message with ID {id}")
         if not file_id:
             logging.debug(f"Message with ID {id} not found")
