5fb4a5915d051c381bc70b6c72faaf950e8057d7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-27 17:47:19 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -5,7 +5,7 @@
 import re
 from pyrogram import filters, Client, enums
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
-from config import ADMINS, LOG_CHANNEL, PUBLIC_FILE_STORE
+from config import ADMINS, LOG_CHANNEL, PUBLIC_FILE_STORE, WEBSITE_URL, WEBSITE_URL_MODE
 from plugins.database import unpack_new_file_id
 from plugins.users_api import get_user, get_short_link
 import re
