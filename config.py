d3ffdfa734f9f9a3203b54953cca6b7ba3604bf7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-27 16:33:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002131110143"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
