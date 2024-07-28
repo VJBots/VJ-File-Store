d29f67d3f7dbb025ad87aa83b46356a4a2d6f85b
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-28 16:19:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -194,12 +194,6 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(button)
                 )
                 filesarr.append(msg)
-                k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this ALL Files/Videos to your Saved Messages and Start Download there</i></b>")
-                await asyncio.sleep(600)
-                for x in filesarr:
-                    await x.delete()
-                await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
-            
             except Exception as e:
                 logger.warning(e, exc_info=True)
                 continue
