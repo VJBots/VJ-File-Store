cc43198c042d089e058c2d5ddd61390d2c9c14cb
Kingqpi <108sawai108@gmail.com>
2024-08-03 15:54:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", "sawai"))
+API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
