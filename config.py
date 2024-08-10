f353dc06d90d1ff40fda165d4aacc0a1aa4bf1cf
Sender749 <jotmahal2003@gmail.com>
2024-08-10 18:40:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ API_ID = int(environ.get("API_ID", "29453152"))
 API_HASH = environ.get("API_HASH", "2302adc174dbc954ae5081eda5131166")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+PICS = (environ.get('PICS', '')).split() # Bot Start Picture
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1052054451').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Hereislinkbot") # without @
 PORT = environ.get("PORT", "8080")
 
