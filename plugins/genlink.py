cc47f01da8d3d96ec3017a013c9e0127613ccae0
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-08-11 20:13:25 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -72,7 +72,7 @@ async def gen_link_s(bot, message):
     user_id = message.from_user.id
     user = await get_user(user_id)
     if WEBSITE_URL_MODE == True:
-        share_link = f"{WEBSITE_URL}?Tech_VJ={outstr}"
+        share_link = f"{WEBSITE_URL}?ANIFLIX={outstr}"
     else:
         share_link = f"https://t.me/{username}?start={outstr}"
     if user["base_site"] and user["shortener_api"] != None:
