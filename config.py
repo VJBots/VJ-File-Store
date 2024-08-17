35a255c5b9c8d5d632cb586322f7241287e676d9
Tgarun123 <tgarun163@gmail.com>
2024-08-17 08:06:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://shaky-deeyn-arunbots-c431e0ba.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
