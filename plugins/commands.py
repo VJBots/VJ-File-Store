6ecda4231c438bb80c5ba80f8fb0b458b2aaa7d8
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-01-10 17:51:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,6 @@ from Script import script
 from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
-from plugins.database import get_file_details
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
 from utils import verify_user, check_token, check_verification, get_token
