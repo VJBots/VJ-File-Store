8a1556b201a7f43d83f34fe3ea259e9d597bd09d
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-01-11 15:50:10 +0530

    Update users_api.py

--- clone_plugins/users_api.py
+++ clone_plugins/users_api.py
@@ -7,14 +7,14 @@
 import requests
 import json
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import CLONE_DB_URI, DB_NAME
+from config import CLONE_DB_URI, CDB_NAME
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 client = AsyncIOMotorClient(CLONE_DB_URI)
-db = client[DB_NAME]
+db = client[CDB_NAME]
 col = db["users"]
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -35,21 +35,16 @@ async def get_short_link(user, link):
 # Ask Doubt on telegram @KingVJ01
 
 async def get_user(user_id):
-
     user_id = int(user_id)
-
     user = await col.find_one({"user_id": user_id})
-
     if not user:
         res = {
             "user_id": user_id,
             "shortener_api": None,
             "base_site": None,
         }
-
         await col.insert_one(res)
         user = await col.find_one({"user_id": user_id})
-
     return user
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -65,28 +60,3 @@ async def update_user_info(user_id, value:dict):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
-async def total_users_count():
-    count = await col.count_documents({})
-    return count
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-async def get_all_users():
-    all_users = col.find({})
-    return all_users
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-async def delete_user(user_id):
-    await col.delete_one({'user_id': int(user_id)})
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
