189e921c904533f6d7a7d5786bfee6db3751e5a1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:01:44 +0530

    Update human_readable.py

--- TechVJ/utils/human_readable.py
+++ TechVJ/utils/human_readable.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
     # 2**10 = 1024
