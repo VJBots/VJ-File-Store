f0ee9e264128b8d955bc3c0f38936edb059f8377
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 12:55:18 +0530

    Update dbusers.py

--- plugins/dbusers.py
+++ plugins/dbusers.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
