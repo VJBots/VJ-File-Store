d90e438582c1f74afaa3fd111423219864b95921
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:00:45 +0530

    Update config_parser.py

--- TechVJ/utils/config_parser.py
+++ TechVJ/utils/config_parser.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from os import environ
 from typing import Dict, Optional
 
