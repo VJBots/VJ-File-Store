46cb474ed7e3492ecb43356b1ec33f1abfa77b81
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:08:08 +0530

    Update __init__.py

--- TechVJ/bot/__init__.py
+++ TechVJ/bot/__init__.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from pyrogram import Client, types
 from config import *
 from typing import Union, Optional, AsyncGenerator
