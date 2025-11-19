c26640341fee9ee12c4401e53852b35b31038957
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:01:29 +0530

    Update file_size.py

--- TechVJ/utils/file_size.py
+++ TechVJ/utils/file_size.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
     """ Returns a human readable string representation of bytes """
     return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
